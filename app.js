7510c7113527208bbbf4b6a9387f58ccc21caa5d
Fatih Sarıkaya <f.sarikaya@gmail.com>
2022-09-14 03:02:54 +0300

    ad

--- app.js
+++ app.js
@@ -5,38 +5,31 @@ const formEl = document.getElementById("form");
 const scoreEl = document.getElementById("score");
 const questionEl = document.getElementById("question");
 const inputEl = document.getElementById("input");
-const btnEl = document.getElementById("btn");
 
-const score =JSON.parse(localStorage.getItem("score"));
+const score = JSON.parse(localStorage.getItem("score"));
 
 if (!score) {
   score = 0;
 }
+const answer = num1 * num2;
 
-scoreEl.innerText = `Score:${score}`;
+scoreEl.innerText = `Score: ${score}`;
 
 questionEl.innerText = `What is ${num1} multiply by ${num2} ?`;
 
-const answer = num1 * num2;
 
 formEl.addEventListener("submit", () => {
-    const userAns = +inputEl.value;
+  const userAns = +inputEl.value;
 
   if (userAns === answer) {
-    score++;    
+    score++;
     updateLocalStorage();
-    
   } else {
-    score--;    
+    score--;
     updateLocalStorage();
   }
 });
 
-
 function updateLocalStorage() {
   localStorage.setItem("score", JSON.stringify(score));
 }
-
-// btnEl.addEventListener("click",()=>{
-//     questionEl.innerText=`What is ${num1} multiply by ${num2} ?`
-// })
